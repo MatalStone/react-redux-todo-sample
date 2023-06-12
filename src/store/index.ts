e48a0c3a973fb5de4cefa5b29d32460953500752
AzureAD\石川真行 <masayuki.ishikawa@thinkings.co.jp>
2023-06-12 13:58:27 +0900

    storeのコメント

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,17 @@
 import { createStore } from "redux";
 
+// ユーザー定義型
 export interface ToDoItem {
   id: number;
   note: string;
 }
 
+// stateの型定義
 export interface ApplicationState {
   toDos: ToDoItem[];
 }
 
+// stateの初期値
 const initialState: ApplicationState = {
   toDos: [
     { id: 1, note: "じゃがいも" },
@@ -18,10 +21,12 @@ const initialState: ApplicationState = {
   ],
 };
 
+// action.typeの文字列
 export const ADD_NOTE = "TODO/ADD_NOTE";
 export const UPDATE_NOTE = "TODO/UPDATE_NOTE";
 export const REMOVE_NOTE = "TODO/REMOVE_NOTE";
 
+// actionの型定義
 export interface AddNote {
   type: typeof ADD_NOTE;
   note: string;
@@ -39,6 +44,7 @@ export interface RemoveNote {
 
 export type KnownAction = AddNote | UpdateNote | RemoveNote;
 
+// reducer
 export const reducer = (
   state: ApplicationState = initialState,
   action: KnownAction
@@ -73,6 +79,8 @@ export const reducer = (
   }
 };
 
+// storeの生成
 const store = createStore(reducer as any);
 
+// export
 export default store;
